c40d8ddcf7cbd1af2966a67b5db6f43291cd7d64
MattiaCarolo <mattia.carolo@studenti.unitn.it>
2022-05-05 22:54:42 +0200

    fixed bug in which final_pop had two types in the same list

--- src/components/ga.py
+++ src/components/ga.py
@@ -1,3 +1,4 @@
+from cgi import parse_multipart
 from secrets import choice
 from inspyred import ec
 import numpy as np
@@ -36,13 +37,13 @@ def cross_over(parents, n_genes):
     while n_genes > 0:
         for i in range(len(parents)):
             print("parent iterator = " , i)
+            print("parent = ", parents )
             if (n <= len(parents[i])):
                 gene = choice(parents[i]) # extract randomly a gene from the parent. These gene will be part of the offspring's genome
                 parents[i].remove(gene) # remove the gene from the parent. In this way we cannot select these gene multiple time for the offspring's genome
                 offspring.append(gene)
                 n_genes -= 1
             print("n_genes = " + str(n_genes))
-        breakpoint()
         n += 1
 
     return offspring
@@ -77,12 +78,19 @@ def evolution(pop_with_eval):
         parents = choices(mutated_pop, weights= prob_sel, k = n_parents) # select n_parents from mutated_pop. Each parent is selected accordingly to prob_sel
         
         maxi = max([len(fract) for fract , _ in parents])
-        n_genes = randint(n_parents, maxi) # determine the number of genes for the offspring. It is selected as a random number between the number of parents and the maximal length of the genome between the parents
+        if(n_parents == maxi):
+            n_genes = n_parents
+        elif(n_parents > maxi):
+             n_genes = randint(maxi, n_parents) # determine the number of genes for the offspring. It is selected as a random number between the number of parents and the maximal length of the genome between the parents
+        elif(n_parents < maxi):
+            n_genes = randint(n_parents, maxi) # determine the number of genes for the offspring. It is selected as a random number between the number of parents and the maximal length of the genome between the parents
         final_pop.append(cross_over([par for par, _ in parents], n_genes)) # generate a new offspring
 
     pop_with_eval.sort(key = lambda x: x[1], reverse = True)
     elite = pop_with_eval[:3] # extract the elite individuals from the current population (pop_with_eval). The elite individuals are selected as the three individuals of the populations that has the higher score
 
-    final_pop = final_pop + elite # add the elite individuals to the individuals obtained from mutation and cross-over
+    for e,_ in pop_with_eval[:3]:
+        final_pop.append(e)
+    #final_pop = final_pop + elite # add the elite individuals to the individuals obtained from mutation and cross-over
 
     return final_pop
